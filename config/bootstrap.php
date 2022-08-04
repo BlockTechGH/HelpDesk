c7e5c0e764d40e3c9260491a3da45ded026d8b49
Evgeniy Lazarev <evgeniy.lazarev@office.e2e4gu.ru>
2022-08-04 16:03:14 +0700

    Include app configuration in bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -221,3 +221,9 @@ TypeFactory::map('time', StringType::class);
 //Inflector::rules('plural', ['/^(inflect)or$/i' => '\1ables']);
 //Inflector::rules('irregular', ['red' => 'redlings']);
 //Inflector::rules('uninflected', ['dontinflectme']);
+
+
+if(file_exists(CONFIG . 'app_config.php'))
+{
+    Configure::load('app_config', 'default');
+}
