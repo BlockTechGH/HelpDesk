4b395580cdbf53fdcd48a74782adbc990a803cba
Evgeniy Lazarev <evgeniy.lazarev@office.e2e4gu.ru>
2023-05-12 14:32:06 +0700

    Renaming by 12/05/2023

--- templates/Bitrix/display_ticket_card.php
+++ templates/Bitrix/display_ticket_card.php
@@ -204,13 +204,13 @@
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                             <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                         </svg>
-                        <?= __('Resolution') ?>
+                        <?= __('Updates and Resolutions') ?>
                     </a>
                     <a class="nav-link" href="#" id="files-tab" data-toggle="tab" data-target="#files" role="tab" aria-controls="files" aria-selected="false">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16">
                             <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
                         </svg>
-                        <?= __('File Attachments') ?>
+                        <?= __('Screenshots and Attachments') ?>
                     </a>
                 </nav>
                 <!-- End menu -->
